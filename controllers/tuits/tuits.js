f306bcc54ea5388299fc1d5bbe2a92154773c113
yingguocs22 <guoyingcs@163.com>
2023-04-02 12:11:24 -0700

    update dislike in tuits

--- controllers/tuits/tuits.js
+++ controllers/tuits/tuits.js
@@ -6,6 +6,7 @@ export default [
         "tuit": "You want to wake up in the morning and think the future is going to be great - and that’s what being a spacefaring civilization is all about. It’s about believing in the future and thinking that the future will be better than the past. And I can’t think of anything more exciting than going out there and being among the stars",
         "liked": true,
         "likes": 2345,
+        "disliked": false,
         "dislikes": 0,
         "replies": 123,
         "retuits": 432
@@ -17,6 +18,7 @@ export default [
     "tuit": "The Boring Company (TBC) is an American infrastructure and tunnel construction services company founded by Elon Musk. Its ongoing and proposed projects are designed for intra-city ('loop') transit systems.",
     "liked": true,
     "likes": 2345,
+    "disliked": false,
     "dislikes": 0,
     "replies": 321,
     "retuits": 432
@@ -28,6 +30,7 @@ export default [
     "tuit": "Neurolink is an American company founded by Elon Musk that specializes in brain to computer integration. The first application will be to cure spinal cord injuries so that paraplegics can walk again. Other applications include curing blindness and mental disorders.",
     "liked": true,
     "likes": 3456,
+    "disliked": false,
     "dislikes": 0,
     "replies": 345,
     "retuits": 654
